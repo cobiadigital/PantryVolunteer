506f650bf2b57dab7fdddda8fae0d79c4441dfb1
cobiadigital <ben.brenner@gmail.com>
2022-04-12 11:58:22 -0500

    fix sig pad2

--- volunteer/static/app.js
+++ volunteer/static/app.js
@@ -1,12 +1,11 @@
 var wrapper = document.getElementById("signature-pad");
 var clearButton = wrapper.querySelector("[data-action=clear]");
 
+
 var canvas = wrapper.querySelector("canvas");
 var signaturePad = new SignaturePad(canvas, {
   // It's Necessary to use an opaque color when saving image as JPEG;
   // this option can be omitted if only saving as PNG or SVG
-  // minWidth: 5,
-  //   maxWidth: 10,
   backgroundColor: 'rgb(255, 255, 255)'
 });
 
@@ -18,25 +17,17 @@ function submitForm() {
 // Adjust canvas coordinate space taking into account pixel ratio,
 // to make it look crisp on mobile devices.
 // This also causes canvas to be cleared.
-function resizeCanvas() {
-  // When zoomed out to less than 100%, for some very strange reason,
-  // some browsers report devicePixelRatio as less than 1
-  // and only part of the canvas is cleared then.
-  var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+// function resizeCanvas() {
+//     const ratio =  Math.max(window.devicePixelRatio || 1, 1);
+//     canvas.width = canvas.offsetWidth * ratio;
+//     canvas.height = canvas.offsetHeight * ratio;
+//     canvas.getContext("2d").scale(ratio, ratio);
+//     signaturePad.clear(); // otherwise isEmpty() might return incorrect value
+// }
+//
+// window.addEventListener("resize", resizeCanvas);
+// resizeCanvas();
 
-  // This part causes the canvas to be cleared
-  canvas.width = canvas.offsetWidth * ratio;
-  canvas.height = canvas.offsetHeight * ratio;
-  canvas.getContext("2d").scale(ratio, ratio);
-
-  // This library does not listen for canvas changes, so after the canvas is automatically
-  // cleared by the browser, SignaturePad#isEmpty might still return false, even though the
-  // canvas looks empty, because the internal data of this library wasn't cleared. To make sure
-  // that the state of this library is consistent with visual state of the canvas, you
-  // have to clear it manually.
+clearButton.addEventListener("click", function (event) {
   signaturePad.clear();
-}
-
-// On mobile devices it might make more sense to listen to orientation change,
-// rather than window resize events.
-window.onresize = resizeCanvas;
+});
